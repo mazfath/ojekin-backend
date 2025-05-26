a7dd210b943b3dbe306577e33671991c62d5e67d
mazfath <mazfath.2811@gmail.com>
2025-05-26 14:26:23 +0700

    Resolve merge conflict

--- config/db.js
+++ config/db.js
@@ -4,7 +4,11 @@ require('dotenv').config();
 const dbUrl = process.env.MYSQL_URL; // Dari Railway
 
 const pool = mysql.createPool({
+
   uri: dbUrl,
+
+  uri: process.env.MYSQL_URL,
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
