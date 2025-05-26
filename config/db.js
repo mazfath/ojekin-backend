a321c576ab08a6f6919fd7c743e6a6b9c99bf748
Mazfath <mazfath.2811@gmail.com>
2025-05-26 13:56:18 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+const dbUrl = process.env.MYSQL_URL; // Dari Railway
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'ojekin',
+  uri: dbUrl,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
