0fc69c65c9d9a08caa2f2f51768822499831951a
Mazfath <mazfath.2811@gmail.com>
2025-05-26 13:20:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const socketHandler = require('./sockets');
 socketHandler(io);
 
 // Jalankan server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
